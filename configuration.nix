53646b1fca24fd16cd0891a248863a71951ccf4b
Ivy-Panda <128657643+Ivy-Panda@users.noreply.github.com>
2023-03-24 23:38:37 -0400

    Enabled polkit

--- configuration.nix
+++ configuration.nix
@@ -92,6 +92,8 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
   
+  security.polkit.enable = true;
+
   # stuff to make fde work
   boot.initrd.luks.devices.cryptroot={device="/dev/disk/by-partlabel/nix";};
 
