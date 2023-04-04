1c27a66599002e4a185ee92c9929833b802c18ed
Ivy-Panda <128657643+Ivy-Panda@users.noreply.github.com>
2023-04-03 23:46:48 -0400

    Added signing options

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -4,5 +4,12 @@
     enable = true;
     userName = "Ivy-Panda";
     userEmail = "128657643+Ivy-Panda@users.noreply.github.com";
+
+    extraConfig = {
+      gpg.format = "ssh";
+      user.signingkey = "~/.ssh/id_ed25519.pub";
+      commit.gpgsign = "true";
+    };
   };
 }
+
