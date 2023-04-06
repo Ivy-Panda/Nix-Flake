0940a20b28345bb0a7217181efff3d4c49575111
Ivy-Panda <128657643+Ivy-Panda@users.noreply.github.com>
2023-04-06 19:05:26 -0400

    Added impermanence input and refactored for future

--- flake.nix
+++ flake.nix
@@ -1,27 +1,35 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.11";
+    impermanence.url = "github:nix-community/impermanence";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, impermanence, home-manager, ... }:
 
-    nixosConfigurations.pandatop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.ivy = import ./home.nix;
-        }
-        
-        ./configuration.nix
-        ./modules/vim.nix
-        ./modules/tmux.nix
-        ./modules/packages.nix
-      ];
+  let
+
+    defaultModules = [
+      ./configuration.nix
+      ./modules/vim.nix
+      ./modules/tmux.nix
+      ./modules/packages.nix
+
+      home-manager.nixosModules.home-manager {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.ivy = import ./home.nix;
+      }
+    ];
+
+  in {
+
+    nixosConfigurations = {
+      pandatop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = defaultModules ++ [];
+      };
     };
   };
 }
