709f0ea379d25a9820b3c6a13de228dc8d77b67b
Ivy-Panda <128657643+Ivy-Panda@users.noreply.github.com>
2023-04-06 13:31:21 -0400

    Cleaned up config

--- configuration.nix
+++ configuration.nix
@@ -1,21 +1,15 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+{ config, pkgs, ... }: {
 
-{ config, pkgs, ... }:
-
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ 
+    ./hardware-configuration.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostId = "12855db8";
-  networking.hostName = "pandatop"; # Define your hostname.
+  networking.hostName = "pandatop";
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
@@ -32,7 +26,6 @@
   i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" ];
   i18n.extraLocaleSettings = { LC_TIME = "C"; }; # Sets the time format to 24h somehow
 
-
   # Use xkbOptions in console
   console.useXkbConfig = true;
 
@@ -46,7 +39,7 @@
     set show-mode-in-prompt on
     set vi-ins-mode-string "\1\e[1;32m\2[i]\1\e[0m\2"
     set vi-cmd-mode-string "\1\e[1;32m\2[\1\e[1;31m\2c\1\e[1;32m\2]\e[0m\2"
-    '';
+  '';
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
@@ -61,22 +54,16 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ivy = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     thunderbird
-  #   ];
+    extraGroups = [ "wheel" ];
   };
 
- # Some programs need SUID wrappers, can be configured further or are
+  # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
   # };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = false;
@@ -87,11 +74,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -99,7 +81,7 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
-  
+
   security.polkit.enable = true;
 
   # stuff to make fde work
@@ -108,7 +90,7 @@
   # Hidpi stuff
   hardware.video.hidpi.enable=true;
   #  services.xserver.dpi=192;
-  
-   # enable flakes
-   nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # enable flakes
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 }
