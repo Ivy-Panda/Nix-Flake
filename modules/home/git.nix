1927b9e230d3607a709a810b6e59d64c9cb7dd7e
Ivy-Panda <128657643+Ivy-Panda@users.noreply.github.com>
2023-04-13 17:11:39 -0400

    Moved ssh config

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,17 +1,5 @@
 { config, nixpkgs, home-manager, ...}: {
 
-  # TODO move the ssh config to a different module
-  programs.ssh = {
-    enable = true;
-
-    matchBlocks.github = {
-      host = "github.com github";
-      hostname = "github.com";
-      user = "git";
-      identityFile = "~/.ssh/keys/github";
-    };
-  };
-
   programs.git = {
     enable = true;
     userName = "Ivy-Panda";
