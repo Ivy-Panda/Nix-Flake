8236feefeba79e86f1a7cb5699bd5e637b88b299
Ivy-Panda <128657643+Ivy-Panda@users.noreply.github.com>
2026-03-04 00:31:01 -0500

    restructured configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,16 +1,40 @@
 { config, pkgs, ... }: {
 
+  # Enable flakes
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  boot = {
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.efi.canTouchEfiVariables = true;
+  loader.systemd-boot.enable = false;
+  loader.efi.canTouchEfiVariables = true;
 
   # Use Lanzaboote for Secure Boot
-  boot.lanzaboote.enable = true;
-  boot.lanzaboote.pkiBundle = "/var/lib/sbctl";
-  boot.lanzaboote.settings.reboot-for-bitlocker = true;
-  
-  # Enable firmware update daemon
-  services.fwupd.enable = true;
+  lanzaboote.enable = true;
+  lanzaboote.pkiBundle = "/var/lib/sbctl";
+  lanzaboote.settings.reboot-for-bitlocker = true;
+
+  # FDE initialization 
+  initrd.luks.devices.cryptroot={device="/dev/disk/by-partlabel/nix";};
+
+  # Don't import zfs pools that were not properly exported
+  zfs = {
+    forceImportAll = false;
+    forceImportRoot = false;
+  };
+};
+
+  # Set your time zone.
+  time.timeZone = "US/Eastern";
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocales = [ "tok/UTF-8" ];
+    extraLocaleSettings = { LC_TIME = "C.UTF-8"; }; # Sets the time format to 24h somehow
+  };
+
+  # Use xkbOptions in console
+  console.useXkbConfig = true;
 
   # Use Lix instead of Nix
   nix.package = pkgs.lixPackageSets.stable.lix;
@@ -18,59 +42,42 @@
   # Allow unfree packages systemwide
   nixpkgs.config.allowUnfree = true;
 
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default
-
   # Define users
   security.sudo.extraConfig = "Defaults lecture=never";
+  security.polkit.enable = true;
 
   users = {
-  mutableUsers = false;
-  
-  users.ivy = {
+    mutableUsers = false;
+
+    users.ivy = {
       isNormalUser = true;
       extraGroups = [ "wheel" "wireshark" ];
       hashedPasswordFile = "/persist/shadow/ivy";
     };
   };
 
-  # Set your time zone.
-  time.timeZone = "US/Eastern";
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocales = [ "tok/UTF-8" ];
-  i18n.extraLocaleSettings = { LC_TIME = "C.UTF-8"; }; # Sets the time format to 24h somehow
+  # Enable services
+  services = {
+  # Enable Tailscale
+  tailscale.enable = true;
 
-  # Use xkbOptions in console
-  console.useXkbConfig = true;
+  # Enable firmware update daemon
+  fwupd.enable = true;
 
   # Configure keymap in X11
-  services.xserver.xkb.layout = "us";
-  services.xserver.xkb.options = "ctrl:swapcaps"; # swaps capslock and ctrl
-
-  # Change bash / anything using readline to use vi mode and show colored mode indicators
-  environment.etc."inputrc".text = ''
-    set editing-mode vi
-    set show-mode-in-prompt on
-    set vi-ins-mode-string "\1\e[1;32m\2[i]\1\e[0m\2"
-    set vi-cmd-mode-string "\1\e[1;32m\2[\1\e[1;31m\2c\1\e[1;32m\2]\e[0m\2"
-  '';
+  xserver.xkb.layout = "us";
+  xserver.xkb.options = "ctrl:swapcaps"; # swaps capslock and ctrl
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  # printing.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # xserver.libinput.enable = true;
+};
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -83,17 +90,30 @@
     interfaces.tailscale0.allowedTCPPorts = [ 9876 ];
   };
 
-  # Enable Wireshark
-  programs.wireshark.enable = true;
-  environment.systemPackages = [ pkgs.wireshark-qt ];
-
-  # Enable Tailscale
-  services.tailscale.enable = true;
+  # Change bash / anything using readline to use vi mode and show colored mode indicators
+  environment.etc."inputrc".text = ''
+    set editing-mode vi
+    set show-mode-in-prompt on
+    set vi-ins-mode-string "\1\e[1;32m\2[i]\1\e[0m\2"
+    set vi-cmd-mode-string "\1\e[1;32m\2[\1\e[1;31m\2c\1\e[1;32m\2]\e[0m\2"
+  '';
 
   # TODO: This is a temporary workaround for Network Manager errors on rebuild
   # github:NixOS/nixpkgs/issues/180175
   systemd.services.NetworkManager-wait-online.enable = false;
   systemd.services.systemd-networkd-wait-online.enable = false;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # Enable Wireshark
+  programs.wireshark.enable = true;
+  environment.systemPackages = [ pkgs.wireshark-qt ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -102,18 +122,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
-
-  security.polkit.enable = true;
-
-  # Stuff to make fde work
-  boot.initrd.luks.devices.cryptroot={device="/dev/disk/by-partlabel/nix";};
-
-  # Don't import zfs pools that were not properly exported
-  boot.zfs = {
-    forceImportAll = false;
-    forceImportRoot = false;
-  };
-
-  # Enable flakes
-  nix.settings.experimental-features = ["nix-command" "flakes"];
 }
